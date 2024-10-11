2bb3e76b21393f68fffd43630446202529185f80
machariamarigi <machariamarigi@gmail.com>
2024-10-11 14:00:27 +0300

    practice gradient with custom colors

---
+++ config2.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        custom_orange: "#ffd9a6",
+        custom_green: "#00cc66",
+        custom_purple: "#990099",
+        custom_pink: "#ff99ff",
+      },
+    },
+  },
+};
